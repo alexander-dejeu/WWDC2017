ef277bc80722f87b56402dc94b9975cb399276bd
Alexander Dejeu <alexdejeu@gmail.com>
2017-04-03 18:32:30 -0700

    Making open source

--- WWDC.playground/Contents.swift
+++ WWDC.playground/Contents.swift
@@ -1,4 +1,4 @@
-//: Playground - noun: a place where people can play
+//: Hello! 👋 I hope you are doing well! 😊  This playground is awesome!! 😎💥 So get hyped!  It does have quite a few files so please be patient when waiting for it to run 🔥 You know it has started when the audio starts and the live view goes white and black text starts appearing 📱 I love feedback - so please feel free to reach me at the provided information! 💝 Open source and would love to hear your feedback! 
 
 import UIKit
 import PlaygroundSupport
@@ -9,5 +9,3 @@ let h : CGFloat = 945
 let view = AYearAtApple(frame: CGRect(x: 0, y: 0, width: w, height: h))
 PlaygroundPage.current.liveView = view
 PlaygroundPage.current.needsIndefiniteExecution = true
-
-
